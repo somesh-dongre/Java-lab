fcb2c7dd5ac1786139481bf1987c04023ba35966
somesh-dongre <101040562+somesh-dongre@users.noreply.github.com>
2022-04-27 23:54:37 +0530

    update

--- lab8/Program1.java
+++ lab8/Program1.java
@@ -1,16 +1,12 @@
 import java.awt.*;
 import javax.swing.*;
-import java.awt.event.*;
 
-class Program1 extends JFrame
+class Program1
 {
-    JButton b1,b2;
-    JLabel l1,l2,l3,l4,l5,l6,l7;
-
     public static void main(String args[])
     {
         JFrame frm=new JFrame("Number Conversion");
-        frm.setSize(400, 200);
+        frm.setSize(600, 600);
         frm.setLayout(new GridLayout(5,2));
 
         JLabel l1=new JLabel("Decimal");
@@ -18,15 +14,12 @@ class Program1 extends JFrame
         t1.setEditable(false);
 
         JLabel l2=new JLabel("Binary");
-        JLabel e1=new JLabel(" ");
         JLabel l3=new JLabel("Result in Binary");
 
         JLabel l4=new JLabel("Octal");
-        JLabel e2=new JLabel(" ");
         JLabel l5=new JLabel("Result in Octal");
 
         JLabel l6=new JLabel("Hexadecimal");
-        JLabel e3=new JLabel(" ");
         JLabel l7=new JLabel("Result in Hexadecimal");
 
         JButton b1=new JButton("Convert");
@@ -36,19 +29,18 @@ class Program1 extends JFrame
         frm.add(t1);
 
         frm.add(l2);
-        frm.add(e1);
         frm.add(l3);
 
         frm.add(l4);
-        frm.add(e2);
         frm.add(l5);
 
         frm.add(l6);
-        frm.add(e3);
         frm.add(l7);
 
         frm.add(b1);
         frm.add(b2);
+
+        frm.setVisible(true);
     }
 }
 
